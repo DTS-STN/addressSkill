3d90443f6f53b60560ae0630bbd0a0dfb4a5ca4c
Kanmi Obasa <kanmi@konfirmed.com>
2022-03-24 13:46:43 -0400

    Update app.ts
    port fix.

--- src/app.ts
+++ src/app.ts
@@ -259,7 +259,7 @@ const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
 
-server.listen(process.env.port || process.env.PORT || 39785, () => {
+server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`\n${server.name} listening to ${server.url}`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
